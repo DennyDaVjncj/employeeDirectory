725bff54ea353b7cb5e58e794b79d1ca4c1b4564
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-04 13:18:44 -0500

    tentative logic

--- employee-directory/App.js
+++ employee-directory/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import{BrowserRouter as Router,Rout}from'react-router-dom';
+import homePage from './src/pages/homePage';
+import Navbar from './src/'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar/>
+        <Wrapper>
+          <Route exact path='/' component={homePage}/>
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
