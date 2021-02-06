1dd6db731d47eecebffb84efa7d237d7af635d51
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-05 23:13:14 -0500

    notes/tentative  logic

--- employee-directory/src/pages/homePage.js
+++ employee-directory/src/pages/homePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const homePage=()=>{
-    return(
+function homePage(){//this is a function based consumer
+    return(//consider defining a component for this page that will handle a sole purpose, like a nav bar that will propegate down to each page
         <div>
+           <Header/> 
             <h1>da Vjncj Enterprises</h1>
             <h3>Where your vision comes to reality!</h3>
         </div>
