438a70528ba240f8ad70af53931035e84c4e9d0c
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-07 02:23:10 -0500

    tentative updates

---
+++ employee-directory/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import HomePage from './HomePage';
+//import Navbar from './Navbar'
+
+function App() {
+  return (
+      <div>
+        {/* <Navbar/> */}
+        {/* <Wrapper> */}
+          <HomePage/>
+        {/* </Wrapper> */}
+      </div>
+  );
+}
+
+export default App;
